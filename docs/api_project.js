191114abd16eab9559587e1f624140435fcc28da
sundowndev <raphael@crvx.fr>
2019-07-17 15:52:52 +0200

    feat: init docs

---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "Semaine intensive 8",
+  "version": "1.0.0",
+  "description": "description",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-07-17T13:52:48.789Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
